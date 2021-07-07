91c012c6adf79be1e7cb4563c36bd98f7927d5ac
Miguel-Armendariz <miguel.armendariz@mail.polimi.it>
2021-07-07 10:01:58 +0200

    Post request and confidence treshold input

--- app.py
+++ app.py
@@ -38,7 +38,8 @@ def index():
                 option = request.form['source']
                 keywords = request.form['keywords']
                 number_images = request.form['number_pic']
-                url_csv = "https://polimi365-my.sharepoint.com/:x:/g/personal/10787953_polimi_it/ETUZnzuvqtlHv0iHyyOI0MYBO7O1yFXIqu0QPeIhHUJZnw?Download=1"
+                #url_csv = "https://polimi365-my.sharepoint.com/:x:/g/personal/10787953_polimi_it/ETUZnzuvqtlHv0iHyyOI0MYBO7O1yFXIqu0QPeIhHUJZnw?Download=1"
+                url_csv = "https://polimi365-my.sharepoint.com/:x:/g/personal/10787953_polimi_it/ETpS3YrdzspLjVs9TGF7JksBSVwPjpVWYKSdEAEqYEMW_w?Download=1"
                 s = {'ID': count, 'source': option, 'keywords': keywords, 'url': url_csv}
                 source_applied[0]= s
                 f = {'ID': "", 'Filter': "", 'Attribute': "", 'url': ""}
@@ -47,12 +48,15 @@ def index():
                 url_csv_get = requests.get(url_csv)
                 url_request = StringIO(url_csv_get.content.decode('utf-8'))
                 df = pd.read_csv(url_request, error_bad_lines=False, index_col=False)
+                file = pd.DataFrame.to_csv(df, path_or_buf=(None))
+                print(df)
                 u = []
                 for x in range(len(df)):
                     p = {"url": df['media_url'].iloc[x]}
                     u.append(p)
                 urls.append(u)
-                paths.append(url_csv_get.text)
+                #paths.append(url_csv_get.text)
+                paths.append(file)
     
                 count+=1
                 
@@ -60,19 +64,22 @@ def index():
                 option = request.form['source']
                 keywords = request.form['keywords']
                 number_images = request.form['number_pic']
-                url_csv = "https://polimi365-my.sharepoint.com/:x:/g/personal/10787953_polimi_it/ETUZnzuvqtlHv0iHyyOI0MYBO7O1yFXIqu0QPeIhHUJZnw?Download=1"
+                #url_csv = "https://polimi365-my.sharepoint.com/:x:/g/personal/10787953_polimi_it/ETUZnzuvqtlHv0iHyyOI0MYBO7O1yFXIqu0QPeIhHUJZnw?Download=1"
+                url_csv = "https://polimi365-my.sharepoint.com/:x:/g/personal/10787953_polimi_it/ETpS3YrdzspLjVs9TGF7JksBSVwPjpVWYKSdEAEqYEMW_w?Download=1"                
                 s = {'ID': count, 'source': option, 'keywords': keywords}
                 source_applied[0]= s
                 
                 url_csv_get = requests.get(url_csv)
                 url_request = StringIO(url_csv_get.content.decode('utf-8'))
                 df = pd.read_csv(url_request, error_bad_lines=False, index_col=False)
+                file = pd.DataFrame.to_csv(df, path_or_buf=(None))
                 u = []
                 for x in range(len(df)):
                     p = {"url": df['media_url'].iloc[x]}
                     u.append(p)
                 urls[0]= u                
-                paths[0]= url_csv_get.text
+                #paths[0]= url_csv_get.text
+                paths[0]= file
                 
         elif 'apply_button' in request.form:
             if int(request.form['apply_button']) == count:
@@ -88,7 +95,8 @@ def index():
                         attribute = [request.form['latitude_text'], request.form['longitude_text']]
                     #url_csv = "https://polimi365-my.sharepoint.com/:x:/g/personal/10787953_polimi_it/EczlUzJfhFdFjwNqc8NThlQB-pYmb6CbxDZbxbwB4xHQCQ?Download=1"
                     params = {'filter_name_list': [attribute.split()[0]],
-                              'confidence_threshold_list': [float(attribute.split()[1])],
+                              #'confidence_threshold_list': [float(attribute.split()[1])],
+                              'confidence_threshold_list': [request.form['confidence']],
                               'column_name': 'media_url',
                               'csv_file': paths[count-1]
                               }
@@ -127,7 +135,8 @@ def index():
                     #url_csv = r"http://131.175.120.2:7777/Filter/API/filterImageURL?filter_name_list=PeopleDetector&filter_name_list=MemeDetector&filter_name_list=PublicPrivateClassifier&confidence_threshold_list=0.98&confidence_threshold_list=0.89&confidence_threshold_list=0.93&column_name=media_url&csv_url=https%3A%2F%2Fdrive.google.com%2Fuc%3Fexport%3Ddownload%26id%3D12hy5NRkFiNG2lI9t6oXQ_12_QDUQz94c"
 
                     params = {'filter_name_list': [attribute.split()[0]],
-                              'confidence_threshold_list': [float(attribute.split()[1])],
+                              #'confidence_threshold_list': [float(attribute.split()[1])],
+                              'confidence_threshold_list': [request.form['confidence']],
                               'column_name': 'media_url',
                               'csv_file': paths[sel_count-1]
                               }                        
@@ -159,14 +168,30 @@ def index():
             urls = []
             paths = []
         elif 'up_button' in request.form:
+            print(applied)
+            print(urls)
+            print(paths)
             #sel_count = int(request.form['up_button'])
             #a = applied[sel_count-2]
             #applied[sel_count-2] = applied[sel_count-1]
             #applied[sel_count-1] = a
             
             #for x in range(count-sel_count+1):
-             pass   
-                
+            #    params = {'filter_name_list': [applied[sel_count-2+x]['Attribute'].split()[0]],
+            #              'confidence_threshold_list': [float(applied[sel_count-2+x]['Attribute'].split()[1])],
+            #              'column_name': 'media_url',
+            #              'csv_file': paths[sel_count-2+x]
+            #             }
+            #    r = requests.post(url='http://131.175.120.2:7777/Filter/API/filterImage', json=params)
+            #    paths[sel_count-1+x] = r.text
+            #    tmp= StringIO(r.text)
+            #    df= pd.read_csv(tmp)
+            #    u = []
+            #    for x in range(len(df)):
+            #        p = {"url": df['media_url'].iloc[x]}
+            #        u.append(p)
+            #    urls[sel_count-1+x]= u   
+            pass
                 
         else:
             #url_download = int(request.form['download_button'])
