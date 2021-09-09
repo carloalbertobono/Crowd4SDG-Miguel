f4a0ebdc8235f851f061ddb70e1a20b51cb5168f
carlobono <carlo.bono@gmail.com>
2021-09-09 18:52:06 +0200

    minors

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 # TAGS for sharing keys and values between templates and code
 
 d = {}
-d['nsfw_tag'] = "Remove not safe for work"
-d['duplicates_tag'] = "Remove near duplicates"
+d['nsfw_tag'] = "Remove not-safe-for-work images"
+d['duplicates_tag'] = "Remove near-duplicate images"
 d['meme'] = "Remove non-photos"
 d['flood_tag'] = "Flood detector"
 
